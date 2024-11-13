32b09a3dd59335aabc47d8ab7bce2d5d720cbc62
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-11-14 04:15:15 +0600

    some css fixed

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='border-t backdrop-blur supports-[backdrop-filter]:bg-background/60'>
+    <footer className='border-t backdrop-blur py-12 supports-[backdrop-filter]:bg-background/60'>
       <div className='container mx-auto px-4 text-center text-gray-400'>
         <p>made with Rakibur Rahman</p>
       </div>
